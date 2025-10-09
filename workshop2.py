46be1f8f9777f150c75b2f184da45daa1cbc8361
chrillesaaf <chrillesaaf@gmail.com>
2025-10-09 14:31:01 +0200

    Incidents affecting more than 100 users:

--- workshop2.py
+++ workshop2.py
@@ -7,6 +7,19 @@ from collections import defaultdict
 with open('network_incidents.csv', encoding='utf-8') as f:
     incidents = list(csv.DictReader(f))
 
+def swedishNumberStringsToFloat(string):
+    # try - try to do something that we know
+    # might give us a runtime error / "crash the program"
+    try:
+        return float(string.replace(' ','').replace(',','.'))
+    # handle exceptions - when what we tried didn't work
+    except:
+        # return 0 if we can't convert to a number
+        return 0
+
+# Create a variable that holds our whole text report
+report = ""
+
 # Create a dictionary where each site maps to a set of weeks
 weeks = defaultdict(set)
 
@@ -19,8 +32,9 @@ for row in incidents:
 weeks = {site: sorted(weeks) for site, weeks in weeks.items()}
 
 # Show result
+report += ('Show sites and analytic period: \n')
 for site, weeks in weeks.items():
-    print(f'"{site}": {weeks}')
+    report += (f'"{site}": {weeks}\n')
 
 # Define the severity levels you want to count
 severity_levels = ['low', 'medium', 'high', 'critical']
@@ -32,6 +46,21 @@ severity_count = {
 }
 
 # Show result
-print('Incident count by severity:')
+report += ('\nIncident count by severity:\n')
 for level, count in severity_count.items():
-    print(f'{level.capitalize()}: {count}')
+    report += (f'{level.capitalize()}: {count}\n')
+
+# Filter incidents where affected_users > 100 using list comprehension
+high_incidents = [
+    row for row in incidents
+    if row['affected_users']. isdigit() and int(row['affected_users']) > 100
+]
+
+# Show result
+report += ('\nIncidents affecting more than 100 users:\n')
+for incident in high_incidents:
+    report += (f'- {incident['ticket_id']}: {incident['description']} ({incident['affected_users']} users)\n')
+
+# write the report to text file
+with open('workshop_2.txt', 'w', encoding='utf-8') as f:
+    f.write(report)
